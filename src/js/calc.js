341410ba709b4e29cf4c71c14dd70716b8831afe
yesthecarlos <carlosmendez86@gmail.com>
2021-04-16 15:56:55 -0700

    WIP: refactoring lifeExpectancy function

--- src/js/calc.js
+++ src/js/calc.js
@@ -11,18 +11,22 @@ export default class AgeChart {
 
   convertToMercury() {
   this.mercury = Math.round(this.earth / .24)
+  return this.mercury
   }
 
   convertToVenus() {
   this.venus = Math.round(this.earth / .62)
+  return this.venus
   }
 
   convertToMars() {
   this.mars = Math.round(this.earth / 1.88)
+  return this.mars
   }
 
   convertToJupiter() {
   this.jupiter = Math.round(this.earth / 11.86)
+  return this.jupiter
   }
 
   lifeExpectancy() {
@@ -30,7 +34,7 @@ export default class AgeChart {
     let userExpectancy = 100
 
     this.expectancy.push(userExpectancy - userAge);
-    this.expectancy.push(userExpectancy.convertToMercury() - userAge.convertToMercury());
+    this.expectancy.push(userAge.convertToMercury());
     this.expectancy.push(userExpectancy.convertToVenus() - userAge.convertToVenus());
     this.expectancy.push(userExpectancy.convertToMars() - userAge.convertToMars());
     this.expectancy.push(userExpectancy.convertToJupiter() - userAge.convertToJupiter());
