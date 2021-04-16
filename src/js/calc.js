18df2e5d53d301326388eae73882e5f0f02342cc
yesthecarlos <carlosmendez86@gmail.com>
2021-04-16 09:59:18 -0700

    passes AgeChart constructor test

--- src/js/calc.js
+++ src/js/calc.js
@@ -1,4 +1,8 @@
 export default function AgeChart(mercury, venus, mars, jupiter, left) {
- 
+  this.mercury = mercury;
+  this.venus = venus;
+  this.mars = mars;
+  this.jupiter = jupiter;
+  this.left = left;
 
 }
